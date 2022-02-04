cd57e9d33a99b9c5c9d73fe3c789677e9cb77369
Tan Wang Leng <tanwangleng@outlook.com>
2022-02-04 16:55:19 +0800

    Add boundaries to terrain

--- src/terrain.rs
+++ src/terrain.rs
@@ -43,4 +43,74 @@ fn setup_terrain(
                 .with_group(GameLayer::World)
                 .with_masks(&[GameLayer::Player, GameLayer::Tower, GameLayer::Bullet]),
         );
+
+    // add invisible barriers around the borders
+    commands
+        .spawn()
+        .insert(Transform {
+            translation: Vec3::new(0.0, 0.0, TERRAIN_SIZE / 2.0),
+            ..Default::default()
+        })
+        .insert(GlobalTransform::default())
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(TERRAIN_SIZE / 2.0, 100.0, 1.0),
+            border_radius: None,
+        })
+        .insert(
+            CollisionLayers::none()
+                .with_group(GameLayer::World)
+                .with_masks(&[GameLayer::Player, GameLayer::Tower, GameLayer::Bullet]),
+        );
+    commands
+        .spawn()
+        .insert(Transform {
+            translation: Vec3::new(0.0, 0.0, -TERRAIN_SIZE / 2.0),
+            ..Default::default()
+        })
+        .insert(GlobalTransform::default())
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(TERRAIN_SIZE / 2.0, 100.0, 1.0),
+            border_radius: None,
+        })
+        .insert(
+            CollisionLayers::none()
+                .with_group(GameLayer::World)
+                .with_masks(&[GameLayer::Player, GameLayer::Tower, GameLayer::Bullet]),
+        );
+    commands
+        .spawn()
+        .insert(Transform {
+            translation: Vec3::new(TERRAIN_SIZE / 2.0, 0.0, 0.0),
+            ..Default::default()
+        })
+        .insert(GlobalTransform::default())
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(1.0, 100.0, TERRAIN_SIZE / 2.0),
+            border_radius: None,
+        })
+        .insert(
+            CollisionLayers::none()
+                .with_group(GameLayer::World)
+                .with_masks(&[GameLayer::Player, GameLayer::Tower, GameLayer::Bullet]),
+        );
+    commands
+        .spawn()
+        .insert(Transform {
+            translation: Vec3::new(-TERRAIN_SIZE / 2.0, 0.0, 0.0),
+            ..Default::default()
+        })
+        .insert(GlobalTransform::default())
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(1.0, 100.0, TERRAIN_SIZE / 2.0),
+            border_radius: None,
+        })
+        .insert(
+            CollisionLayers::none()
+                .with_group(GameLayer::World)
+                .with_masks(&[GameLayer::Player, GameLayer::Tower, GameLayer::Bullet]),
+        );
 }
